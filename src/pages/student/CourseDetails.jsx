fc5f320814f06440f1493303c4b1357131de238b
sidimed <sidimedm76@gmail.com>
2025-06-20 13:40:58 +0000

    course details page - part 1

--- src/pages/student/CourseDetails.jsx
+++ src/pages/student/CourseDetails.jsx
@@ -1,10 +1,89 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../../context/AppContext";
+import Loading from "../../components/student/Loading";
+import { assets } from "../../assets/assets";
 
 const CourseDetails = () => {
-  return (
-    <div>
-      <h1>Course Details page</h1>
-    </div>
+  const { id } = useParams();
+
+  const [courseData, setCourseData] = useState(null);
+
+  const { allCourses, calculateRating } = useContext(AppContext);
+
+  useEffect(() => {
+    if (allCourses && allCourses.length > 0 && id) {
+      const fetchCourseData = async () => {
+        const findCourse = allCourses.find((course) => course._id === id);
+        if (findCourse) {
+          setCourseData(findCourse);
+        } else {
+          console.warn("Cours non trouvé avec l'ID :", id);
+        }
+      };
+
+      fetchCourseData();
+    }
+  }, [allCourses, id]);
+
+  return courseData ? (
+    <>
+      <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left">
+        <div
+          className="absolute top-0 left-0 w-full z-0 bg-gradient-to-b from-cyan-100/70"
+          style={{ height: "500px" }}
+        ></div>
+
+        {/* left column */}
+        <div className="max-w-xl z-10 text-gray-500">
+          <h1 className="text-[26px] leading-[36px] md:text-[36px] md:leading-[44px] font-semibold text-gray-800">
+            {courseData.courseTitle}
+          </h1>
+
+          <p
+            className="pt-4 md:text-base text-sm"
+            dangerouslySetInnerHTML={{
+              __html: courseData.courseDescription.slice(0, 200),
+            }}
+          ></p>
+
+          {/* course review and rating */}
+          <div className="flex items-center space-x-2 pt-3 pb-1 text-sm">
+            <p>{calculateRating(courseData)}</p>
+            <div className="flex">
+              {[...Array(5)].map((_, i) => (
+                <img
+                  className="w-3.5 h-3.5"
+                  key={i}
+                  src={
+                    i < Math.floor(calculateRating(courseData))
+                      ? assets.star
+                      : assets.star_blank
+                  }
+                  alt=""
+                />
+              ))}
+            </div>
+            <p className="text-blue-600">
+              ({courseData.courseRatings.length}{" "}
+              {courseData.courseRatings.length > 1 ? "ratings" : "rating"})
+            </p>
+            <p>
+              {courseData.enrolledStudents.length}{" "}
+              {courseData.enrolledStudents.length > 1 ? "students" : "student"}
+            </p>
+          </div>
+          <p className="text-sm">
+            Course by{" "}
+            <span className="text-blue-600 underline">Abdallahi Nah</span>
+          </p>
+        </div>
+        {/* right column */}
+        <div></div>
+      </div>
+    </>
+  ) : (
+    <Loading />
   );
 };
 
