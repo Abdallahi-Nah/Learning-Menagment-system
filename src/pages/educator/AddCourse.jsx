03fb93f5dbdb60932c5a09aab4b88a7b7989536d
sidimed <sidimedm76@gmail.com>
2025-06-21 14:58:26 +0000

    add course page - part 1

--- src/pages/educator/AddCourse.jsx
+++ src/pages/educator/AddCourse.jsx
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import uniqid from "uniqid";
+import Quill from "quill";
 
 const AddCourse = () => {
+  const quillRef = useRef(null);
+  const editorRef = useRef(null);
+
+  const [courseTitle, setCourseTitle] = useState("");
+  const [coursePrice, setCoursePrice] = useState(0);
+  const [discount, setDiscount] = useState(0);
+  const [image, setImage] = useState(null);
+  const [chapters, setChapters] = useState([]);
+  const [showPopup, setShowPopup] = useState(false);
+  const [currentChapterId, setCurrentChapterId] = useState(null);
+  const [lectureDetails, setLectureDetails] = useState({
+    lectureTitle: "",
+    lectureDuration: "",
+    lectureUrl: "",
+    isPreviewFree: false,
+  });
+
+  useEffect(() => {
+    if (!quillRef.current && editorRef.current) {
+      quillRef.current = new Quill(editorRef.current, {
+        theme: "snow",
+      });
+    }
+  }, []);
+
   return (
-    <div>
-      <h1>Add Course</h1>
+    <div className="h-screen overflow-scroll flex flex-col items-start justify-between md:p-8 md:pb-0 p-4 pt-8 pb-0">
+      <form>
+        <div className="flex flex-col gap-1">
+          <p>Course Title</p>
+          <input
+            onChange={(e) => setCourseTitle(e.target.value)}
+            value={courseTitle}
+            type="text"
+            placeholder="Type here"
+            className="outline-none md:py-2.5 py-2 px-2 rounded border border-gray-500"
+            required
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <p>Course Description</p>
+          <div ref={editorRef}></div>
+        </div>
+      </form>
     </div>
   );
 };
